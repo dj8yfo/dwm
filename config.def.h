2c909f51f89838dc4afc42078910c61af09e83a9
djeaunty8fourmule <sffhox@gmail.com>
2020-08-15 22:22:01 +0300

    awesome bar

--- config.def.h
+++ config.def.h
@@ -20,6 +20,7 @@ static const char *colors[][3]      = {
         /*               fg         bg         border   */
         [SchemeNorm] = { col_white, col_gray2, col_cyan },
         [SchemeSel]  = { col_white, col_black,  col_green  },
+        [SchemeHid]  = { col_cyan,  col_gray1, col_cyan  },
 };
 
 /* tagging */
@@ -120,6 +121,7 @@ static Button buttons[] = {
         /* click                event mask      button          function        argument */
         { ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
         { ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+        { ClkWinTitle,          0,              Button1,        togglewin,      {0} },
         { ClkWinTitle,          0,              Button2,        zoom,           {0} },
         { ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
         { ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
